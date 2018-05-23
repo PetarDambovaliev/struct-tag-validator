f6848074e96233d96b7699bb1ffb1d06528e641d
PetarDambovaliev <petar.atanasov.1987@gmail.com>
2018-05-24 00:55:55 +0200

    initial commit

---
+++ tag.go
@@ -0,0 +1,197 @@
+package validator
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"sync"
+	"errors"
+	"fmt"
+)
+
+// Tag represents a model struct tag.
+type Tag struct {
+	name string
+	value string
+	structName string
+}
+
+// GetName returns the name of the tag.
+func (t *Tag) GetName() string  {
+	return t.name
+}
+
+// GetValue returns the value of the tag.
+func (t *Tag) GetValue() string  {
+	return t.value
+}
+
+// GetStructName returns the struct name the tag belongs to.
+func (t *Tag) GetStructName() string  {
+	return t.structName
+}
+
+func getPackages(folder string, models ...string) map[string]*ast.Package {
+	var path string
+
+	path = os.Getenv("GOPATH")
+	path = filepath.Join(path, "src")
+	path = filepath.Join(path, folder)
+
+
+	fset := token.NewFileSet()
+	modelMap := make(map[string]bool, len(models))
+
+	for _, model := range models {
+		k := strings.Join([]string{
+			strings.ToLower(model),
+			"go",
+		}, ".")
+
+		modelMap[k] = true
+	}
+
+	pkgs, err := parser.ParseDir(fset, path, func(f os.FileInfo) bool {
+		isNotTest := !strings.HasSuffix(f.Name(), "_test.go")
+
+		if len(modelMap) > 0 {
+			_, exists := modelMap[strings.ToLower(f.Name())]
+
+			return isNotTest != !exists
+		}
+
+		return isNotTest
+	}, 0)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if len(pkgs) == 0 {
+		panic(errors.New(fmt.Sprintf("No structs found at %v", path)))
+	}
+
+	return pkgs
+}
+
+func getTags(tagNames []string, packages map[string]*ast.Package) map[string][]*Tag {
+
+	concatNames := strings.Join(tagNames, "|")
+
+	for _, name := range tagNames {
+		if name == AllTags {
+			concatNames = "[a-z0-9_]+"
+			break
+		}
+	}
+
+	var dbRegex = regexp.MustCompile(
+		strings.Join([]string{
+			"(",
+			concatNames,
+			")",
+			`[ ]*:[ ]*"([^"]*)"`},
+			"",
+			),
+		)
+
+	tagChans := []<-chan *Tag{}
+	tags := map[string][]*Tag{}
+
+	for _, pkg := range packages {
+		for _, file := range pkg.Files {
+			tagChan := collecFields(file, dbRegex)
+			tagChans = append(tagChans, tagChan)
+		}
+	}
+
+	t := multiplex(tagChans...)
+
+	 Loop: for {
+		select {
+			case tag, ok := <-t:
+				if !ok {
+					break Loop
+				}
+
+				tags[tag.structName] = append(tags[tag.structName], tag)
+		}
+	}
+
+	return tags
+}
+
+func multiplex(cs ...<-chan *Tag) <-chan *Tag {
+	var wg sync.WaitGroup
+	out :=  make(chan *Tag, 50 * len(cs))
+
+	output := func(c <- chan *Tag) {
+		defer wg.Done()
+
+		 for {
+			select {
+				case tag := <- c:
+					if tag == nil {
+						return
+					}
+
+					out <- tag
+			}
+		}
+	}
+
+	wg.Add(len(cs))
+
+	for _, c := range cs {
+		go output(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
+
+func collecFields(file *ast.File, dbRegex *regexp.Regexp) <-chan *Tag {
+
+	tagChan := make(chan *Tag, 50)
+	var structName string
+
+		go func() {
+			ast.Inspect(file, func(node ast.Node) bool {
+				switch x := node.(type) {
+				case *ast.TypeSpec:
+					//Get the struct name and end pos
+					structName = x.Name.Name
+				case *ast.StructType:
+					//Extract all db tags from the struct fields
+					for _, field := range x.Fields.List {
+						if field.Tag != nil {
+							matches := dbRegex.FindAllStringSubmatch(field.Tag.Value, -1);
+							if len(matches) > 0 {
+								for _, matchTags := range matches {
+									tagChan <- &Tag{
+										matchTags[1],
+										matchTags[2],
+										structName,
+									}
+								}
+							}
+						}
+					}
+				}
+
+				return true
+			})
+
+			tagChan <- nil
+		}()
+
+	return tagChan
+}
---
+++ validator.go
@@ -0,0 +1,164 @@
+package validator
+
+import (
+	"fmt"
+	"go/ast"
+	"strings"
+	"regexp"
+	"errors"
+)
+
+const AllTags = "*"
+
+var defaultRegexRules = map[string]*regexp.Regexp{
+	//allowed symbols in a tag
+	"Invalid symboles %v in %v.%v.%v": regexp.MustCompile(`[^a-z0-9_, ]+`),
+	//allowed symbols of the end of a tag
+	"Tag cannot end on %v in  %v.%v.%v" : regexp.MustCompile(`[^a-z0-9]$`),
+}
+
+// Validator holds information about the parsed models
+type Validator struct {
+	packages map[string]*ast.Package
+	tags     map[string][]*Tag
+	processors map[string][]func(tag *Tag) []error
+	path string
+	allowDuplicates bool
+}
+
+// AddDefaultProcessors provides some basic processors that will validate the given model tags.
+// The tags given for the processors will be the tags parsed by the validator,`*` is a reference to all tags.
+// If no tags were specified all tags will be parsed and validated.
+func (v *Validator) AddDefaultProcessors(tags ...string) {
+
+	if len(tags) == 0 {
+		tags = []string{AllTags}
+	}
+
+	for _, tagStr := range tags {
+		v.processors[tagStr] = append(v.processors[tagStr], func(tag *Tag) []error {
+			errs := []error{}
+
+			for msg, rexpr := range defaultRegexRules {
+				match := rexpr.FindString(tag.value)
+
+				if len(match) > 0 {
+					errs = append(errs, errors.New(fmt.Sprintf(msg, match, tag.GetStructName(), tag.GetName(), tag.GetValue())))
+				}
+			}
+
+			return errs
+		})
+
+		v.processors[tagStr] = append(v.processors[tagStr], func(tag *Tag) []error {
+			errs := []error{}
+
+			if len(tag.value) == 0 {
+				errs = append(errs, errors.New(fmt.Sprintf("Tag cannot be empty %v.%v", tag.GetStructName(), tag.GetName())))
+			}
+
+			return errs
+		})
+	}
+}
+
+// checkForDuplicates validates duplicate tag values
+func checkForDuplicates(t *Tag, fieldsCache map[string]bool) []error {
+	errs := []error{}
+	cacheKey := strings.Join([]string{t.GetName(), t.GetName(), t.GetValue()}, ".")
+
+	if _, exist := fieldsCache[cacheKey]; exist {
+		errs = append(errs, errors.New(fmt.Sprintf("Duplicate tag value %v in %v.%v", t.GetValue(), t.GetStructName(), t.GetName())))
+	}
+
+	fieldsCache[cacheKey] = true
+
+	return errs
+}
+
+func (v *Validator) setPath(path string)  {
+	v.path = path
+}
+
+// SetAllowDuplicates sets a flag if duplicates are allowed or not.
+// This will determine whether the duplicates check will be run.
+func (v *Validator) SetAllowDuplicates(allowDuplicates bool) {
+	v.allowDuplicates = allowDuplicates
+}
+
+// NewValidator creates a new validator model.
+// It requires a path to the models folder.
+func NewValidator(path string) Validator {
+	m := Validator{}
+	m.setPath(path)
+	m.processors = map[string][]func(tag *Tag) []error{}
+	m.allowDuplicates = false
+
+	return m
+}
+
+// Run  will validate specified tags on all models, if none were passed.
+// It returns validation errors, if any produced by the processor.
+func (v *Validator) Run(models ...string)  []error {
+	v.packages = getPackages(v.path, models...)
+
+	if len(v.processors) == 0 {
+		return []error{
+			errors.New( "There are no processors to run, consider adding the default ones."),
+		}
+	}
+
+	tags := []string{}
+
+	for tag, _ := range v.processors {
+		tags = append(tags, tag)
+	}
+
+	v.tags = getTags(tags, v.packages)
+
+	return  v.validate()
+}
+
+func (v *Validator) validate() []error {
+	fieldsCache := map[string]bool{}
+	errs := []error{}
+
+	if len(v.tags) == 0 {
+		return []error{errors.New("No tags found")}
+	}
+
+	for _, fields := range v.tags {
+		for _, t := range fields {
+			executableProcessors := []func(tag *Tag) []error {}
+
+			if !v.allowDuplicates {
+				errs = append(errs, checkForDuplicates(t, fieldsCache)...)
+			}
+
+			processors, exists := v.processors[t.name]
+
+			if exists {
+				executableProcessors = append(processors)
+			}
+
+			globalProcessors, exists := v.processors[AllTags]
+
+			if exists {
+				executableProcessors = append(executableProcessors, globalProcessors...)
+			}
+
+			for _, processor := range executableProcessors {
+				errs = append(errs, processor(t)...)
+			}
+		}
+	}
+
+	return errs
+}
+
+
+// AddProcessor adds a processor that will validate the given model tags
+// The tags given for the processors will be the tags parsed by the validator where `*` is a reference to all tags
+func (v *Validator) AddProcessor(tag string, processor func(t *Tag) []error) {
+	v.processors[tag] = append(v.processors[tag], processor)
+}
